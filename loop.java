059a421be42c70499b4d5dbb7d8512a4fa947946
Maksim Lesko <leskom92@gmail.com>
2017-05-18 22:29:44 +0300

    change for the loop

--- loop.java
+++ loop.java
@@ -8,9 +8,9 @@ public class Factorial
 	
 	public static int factorial(int n)
 	{	int result = 1;
-		for(int i = 2; i <= n; i++)
+		for(int i = 1; i <= n; i++)
 			result *= i;
 		return result;
 	}
 }
-//comment1
+//comment
